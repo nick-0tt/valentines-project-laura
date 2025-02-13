c743bbf0f240146d755c542887d697d8e5f113da
waitplease-code <herr.nicholas.ott2@gmail.com>
2025-02-13 17:31:09 -0300

    calm valentines day

---
+++ script.js
@@ -0,0 +1,28 @@
+function switchAnswer() {
+    const yes = document.getElementById('yes-label');
+    const no = document.getElementById('no-label');
+    const yesValue = document.getElementById('yes');
+    const noValue = document.getElementById('no');
+    console.log(noValue);
+    if (document.querySelector('input:checked').value === "no") {
+        noValue.setAttribute('id', 'yes');
+        noValue.setAttribute('value', 'yes');
+        yesValue.setAttribute('id', 'no');
+        yesValue.setAttribute('value', 'no');
+
+        if (yes.innerText === 'No') {
+            yes.innerText = 'Yes';
+            no.innerText = 'No';
+        } else {
+            yes.innerText = 'No';
+            no.innerText = 'Yes';
+        }
+    } 
+    console.log(document.querySelector('input:checked').value);
+}
+
+function toggleMessage() {
+    const message = document.getElementById('love');
+    console.log("dog");
+    message.classList.toggle('hidden');
+}
